034910b127bbfb928c619116297067d1780a46f7
quantivepx <btwnahid@gmail.com>
2025-10-20 14:17:33 +0530

    ci: test ci workflows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import HeroSection from "@/components/Hero";
 import IconSection from "@/components/IconSection";
-import Navbar from "@/components/Navbar";
-
-const page = () => {
-	return (
 		<div className="from-bgDark to-bgDark relative min-h-screen overflow-hidden bg-gradient-to-br via-zinc-900">
 			<Navbar />
 			<HeroSection />
