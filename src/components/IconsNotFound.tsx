cadddf6046f4e759cf6c948560dbde745e9d902e
GitHub Action <action@github.com>
2025-12-27 08:26:41 +0000

    style: format code with prettier

--- src/components/IconsNotFound.tsx
+++ src/components/IconsNotFound.tsx
@@ -9,7 +9,7 @@ const IconsNotFound: React.FC = () => {
 			transition={{ duration: 0.45, ease: "easeOut" }}
 			className="border-primary/20 bg-primary/10 mx-auto mt-12 flex max-w-sm flex-col items-center gap-2 rounded-lg border px-6 py-6 text-center shadow-lg backdrop-blur"
 		>
-			<h2 className="text-zinc-300 text-sm font-semibold tracking-wide">
+			<h2 className="text-sm font-semibold tracking-wide text-zinc-300">
 				No icons found
 			</h2>
 
